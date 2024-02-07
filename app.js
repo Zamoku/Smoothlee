bf7ee4c07f1cd61b38abac1ff92b0f275c3d0491
Nomzamo Muleka <nomzamo.muleka@wundermanthompson.com>
2024-02-07 05:48:09 +0200

    latest changes

--- app.js
+++ app.js
@@ -125,14 +125,14 @@ menuItems.forEach((item, index) => {
 //   });
 // });
 
-const buyButton = document.querySelector(".buyButton");
-const payment = document.querySelector(".payment");
-const close = document.querySelector(".close");
+// const buyButton = document.querySelector(".buyButton");
+// const payment = document.querySelector(".payment");
+// const close = document.querySelector(".close");
 
-buyButton.addEventListener("click", () => {
-  payment.style.display = "flex";
-});
+// buyButton.addEventListener("click", () => {
+//   payment.style.display = "flex";
+// });
 
-close.addEventListener("click", () => {
-  payment.style.display = "none";
-});
+// close.addEventListener("click", () => {
+//   payment.style.display = "none";
+// });
